4a27d3bdcfef3b86690e9b7ff9dc2db6900fecc8
Max Baylis <baylism@tcd.ie>
2018-09-09 07:47:41 +0100

    move scripts and addscehduelrconfig

---
+++ src/main/java/com/bcam/chainproducer/SchedulerConfiguration.java
@@ -0,0 +1,25 @@
+package com.bcam.chainproducer;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@EnableAsync
+@Configuration
+@EnableScheduling
+public class SchedulerConfiguration {
+// public class SchedulerConfiguration {
+
+
+    // this one works
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(5);
+        return taskScheduler;
+    }
+
+}
